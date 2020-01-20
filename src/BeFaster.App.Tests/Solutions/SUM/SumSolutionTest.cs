8985436fbceefcc0254bdaf9ea60bbc40deda064
sbx_user1051 <sbx_user1051@169.254.217.37>
2020-01-20 08:34:05 +0000

    Mon Jan 20 08:34:05 UTC 2020

--- src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/SUM/SumSolutionTest.cs
@@ -11,5 +11,40 @@ namespace BeFaster.App.Tests.Solutions.SUM
         {
             return SumSolution.Sum(x, y);
         }
+        [TestCase(100, 100, ExpectedResult = 200)]
+        public int Sum_GivenBothatUpperRange_ReturnSum(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+        [TestCase(0, 0, ExpectedResult = 200)]
+        public int CSum_GivenBothatUpperRange_ReturnSum(int x, int y)
+        {
+            return SumSolution.Sum(x, y);
+        }
+        [TestCase(-1,50,ExpectedResult =Exception.OutofRange)]
+        public int Sum_GivenFirstValueBelowRange_ThrowException(int x, int y)
+        {
+
+        }
+
+        [TestCase(101, 50, ExpectedResult = Exception.OutofRange)]
+        public int Sum_GivenFirstValueAboveRange_ThrowException(int x, int y)
+        {
+
+        }
+
+        [TestCase(50, -1, ExpectedResult = Exception.OutofRange)]
+        public int Sum_GivenSecondValueBelowRange_ThrowException(int x, int y)
+        {
+
+        }
+
+        [TestCase(50, 101, ExpectedResult = Exception.OutofRange)]
+        public int Sum_GivenSecondValueAboveRange_ThrowException(int x, int y)
+        {
+
+        }
+
     }
 }
+
