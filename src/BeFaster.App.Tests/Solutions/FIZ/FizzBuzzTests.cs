92377af9b4d9023f734cfeec3d95a732a87b88b6
sbx_user1051 <sbx_user1051@169.254.217.37>
2020-01-20 10:28:15 +0000

    Mon Jan 20 10:28:15 UTC 2020

--- src/BeFaster.App.Tests/Solutions/FIZ/FizzBuzzTests.cs
+++ src/BeFaster.App.Tests/Solutions/FIZ/FizzBuzzTests.cs
@@ -10,15 +10,15 @@ namespace BeFaster.App.Tests.Solutions.FIZ
     {
 
         [TestCase(1, ExpectedResult = "1")]
-        [TestCase(3, ExpectedResult = "fizz")]
+        [TestCase(3, ExpectedResult = "fizz fake deluxe")]
         [TestCase(6, ExpectedResult = "fizz")]
-        [TestCase(5, ExpectedResult = "buzz")]
+        [TestCase(5, ExpectedResult = "buzz fake deluxe")]
         [TestCase(10, ExpectedResult = "buzz")]
-        [TestCase(15, ExpectedResult = "fizz buzz")]
-        [TestCase(1501,ExpectedResult ="buzz")]
+        [TestCase(15, ExpectedResult = "fizz buzz fake deluxe")]
+        [TestCase(1501,ExpectedResult ="buzz fake deluxe")]
         [TestCase(1301,ExpectedResult = "fizz")]
         [TestCase(1531,ExpectedResult ="fizz buzz")]
-        [TestCase(330, ExpectedResult = "fizz fake deluxe")]
+        [TestCase(330, ExpectedResult = "fizz buzz deluxe")]
         [TestCase(1111,ExpectedResult ="1111")]
         [TestCase(2222,ExpectedResult = "2222")]
         [TestCase(444,ExpectedResult ="fizz")]
@@ -32,6 +32,7 @@ namespace BeFaster.App.Tests.Solutions.FIZ
 
     }
 }
+
 
 
 
