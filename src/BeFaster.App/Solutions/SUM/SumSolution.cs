e5f1836f259b7e5771dca4fce2ddcc778198d364
sbx_user1051 <sbx_user1051@169.254.217.37>
2020-01-20 08:43:05 +0000

    Mon Jan 20 08:43:05 UTC 2020

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -1,4 +1,5 @@
 ﻿using BeFaster.Runner.Exceptions;
+using System;
 
 namespace BeFaster.App.Solutions.SUM
 {
@@ -6,8 +7,18 @@ namespace BeFaster.App.Solutions.SUM
     {
         public static int Sum(int x, int y)
         {
+            if(!TestRange(x,0,100))
+            {
+                throw new ArgumentOutOfRangeException; 
+            }
             return x + y;
+        }
+
+        private static bool TestRange (int number, int bottomlimit, int toplimit)
+        {
+            return (number >= bottomlimit && number <= toplimit);
         }
     }
 }
+
 
